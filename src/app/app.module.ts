d0e022373b58cb7fe6f8ed5e4618bf334232ef00
sbriffaud <simon.briffaud@orange.fr>
2021-11-18 23:33:57 +0100

    Ajout du projet existant

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SideMenuComponent,
+    AccueilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/components/side-menu/side-menu.component.ts
@@ -0,0 +1,64 @@
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css']
+})
+export class SideMenuComponent implements OnInit {
+  
+  isMenuOpened: Boolean;
+
+  sidebarWidth: String;
+  sidebarOpacity: Number;
+  animationName: String;
+  animationIterationCount: Number;
+  animationTimingFunction: String;
+  animationDuration: String;
+  deconnectionSectionPaddingRight: String;
+
+  constructor(private render: Renderer2, private elem: ElementRef) {
+    this.isMenuOpened = false;
+
+    this.sidebarWidth = '3.5vw';
+    this.sidebarOpacity = 0;
+    this.deconnectionSectionPaddingRight = '2vw';
+
+    this.animationName = "";
+    this.animationIterationCount = 1;
+    this.animationTimingFunction = "";
+    this.animationDuration = "";
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu(): void {
+
+    if(this.isMenuOpened == true) {
+      this.sidebarWidth = '3.5vw';
+
+      this.sidebarOpacity = 0;
+      this.animationName = "fadeOutOpacity";
+      this.animationIterationCount = 1;
+      this.animationTimingFunction = "ease-in";
+      this.animationDuration = "0.2s";
+
+      this.deconnectionSectionPaddingRight = '2vw';
+    }
+    else if(this.isMenuOpened == false) {
+      this.sidebarWidth = '15vw';
+
+      this.sidebarOpacity = 1;
+      this.animationName = "fadeInOpacity";
+      this.animationIterationCount = 1;
+      this.animationTimingFunction = "ease-in";
+      this.animationDuration = "0.6s";
+
+      this.deconnectionSectionPaddingRight = '0%';
+    }
+
+    this.isMenuOpened = !this.isMenuOpened;
+  }
+
+}
